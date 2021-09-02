99d8a2fb42e086c7b4b28270cddb0d3c9f783c4f
ekanthan.b <ekanthan.b@zohocorp.com>
2021-09-02 11:28:05 +0530

    changes as per the instructions from fiancée.

--- js/script.js
+++ js/script.js
@@ -1,54 +1,3 @@
-(function ($) {
-    "use strict";
-      $('.sakura-falling').sakura();
-})(jQuery);
-
-/**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
- */
-$(document).on('click', function(){
-    document.getElementById("my_audio").play();
-});
-
-// Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
-    }
-}, 1000);
-
 // being a bit cool :p  
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
@@ -80,3 +29,15 @@ var styles2 = [
     , 'font-weight: bold'
     , 'font-size: 32px'
 ].join(';');
+
+function handleTickInit(tick) {
+    var counter = Tick.count.down('2021-09-09T07:31:00+05:30');
+
+    counter.onupdate = function(value) {
+      tick.value = value;
+    };
+
+    counter.onended = function() {
+        tick.root.style.display = 'none';
+    };
+}
